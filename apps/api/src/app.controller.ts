007ca02b66b43b4818901f5803d58973e025eb1c
lucasshuan <jlsrocha1@gmail.com>
2026-04-24 17:33:07 -0300

    fix: mozilla favicon noise

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Head } from '@nestjs/common';
+import { Controller, Get, Head, HttpCode, HttpStatus } from '@nestjs/common';
 
 @Controller()
 export class AppController {
@@ -14,4 +14,8 @@ export class AppController {
   getHealth() {
     return { status: 'ok' };
   }
+
+  @Get('favicon.ico')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  getFavicon() {}
 }
